a89773674e4ed18e45228fda786303e384902eb1
Guillaume Lesaine <guillaume.lesaine@protonmail.com>
2019-02-10 18:09:58 +0100

    Data Loader 1.0
     - data_store with data and methods
     - data_loader function
     - Event listener on DOMContentLoaded

--- static/js/data_loader.js
+++ static/js/data_loader.js
@@ -12,17 +12,40 @@ load_data_button.addEventListener("click", function() {
 // It will be initialize in the future with the a vitrine resume
 data_store = {
   "Account Status History.csv": [],
-  "Connections.csv": [],
+  "Connections.csv": {
+    "data": [],
+    "methods": [plot_connections]
+  },
   "Education.csv": [],
   "Endorsement Received Info.csv": [],
   "Languages.csv": [],
   "Phone Numbers.csv": [],
   "Positions.csv": [],
-  "Profile.csv": [],
+  "Profile.csv": {
+    "data": [],
+    "methods": [plot_header]
+  },
   "Projects.csv": [],
   "Skills.csv": [],
   "Test Scores.csv": []
 }
+
+//Function loading the initial data
+var data_loader = function(name_csv) {
+  d3.csv("data/Data_L/" + name_csv, function(raw) {
+    data_store[name_csv].data = raw;
+    data_store[name_csv].methods.forEach(function(method) {
+      method(data_store[name_csv].data)
+    })
+  });
+}
+
+//Load the initial data on loading of the page
+document.addEventListener('DOMContentLoaded',
+  function() {
+    data_loader("Profile.csv")
+    data_loader("Connections.csv")
+  }, false);
 
 // We store in a list the files that need additional processing using custom regex
 files_additional_treatment = ["Account Status History.csv", "Endorsement Received Info.csv"]
@@ -32,16 +55,20 @@ const input = document.querySelector('input[type="file"]')
 input.addEventListener("change", function(e) {
   for (var i = 0; i < input.files.length; i++) {
     (function(file) {
+      console.log("Loop")
       var name = file.name; // Name of the file
       var reader = new FileReader(); // Use of the FileReader tool
       reader.onload = function(e) {
+        console.log("Loop 2")
         // Check if the name of the file is in the list of the files to process more
         if (files_additional_treatment.indexOf(name) > -1) {
           // If so provide custom regex treatment
-
         } else {
           // Otherwise parse csv to object using JQUERY csv parse
-          data_store[name] = $.csv.toObjects(e.target.result)
+          data_store[name].data = $.csv.toObjects(e.target.result)
+          data_store[name].methods.forEach(function(method) {
+            method(data_store[name].data)
+          })
         }
       }
       reader.readAsText(file, "UTF-8");
