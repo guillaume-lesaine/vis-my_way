9f89713e8584fc2f94daef0d67b7a118d7fc2981
LPeuziat <46816315+LPeuziat@users.noreply.github.com>
2019-02-15 19:14:28 +0100

    timeline update

--- static/js/timeline.js
+++ static/js/timeline.js
@@ -3,26 +3,45 @@ function plot_timeline(data_timeline) {
 	data_positions = data_timeline[1]
 	data_projects = data_timeline[2]
 	
+	//creation data_timeline
+	var data_timeline = [];
+	data_education.forEach(function(d) {
+		d.type = "education";
+		data_timeline.push(d);
+	})
+	data_positions.forEach(function(d) {
+		d.type = "positions";
+		data_timeline.push(d)
+	})
+	data_projects.forEach(function(d) {
+		d.type = "projects";
+		data_timeline.push(d)
+	})
+	console.log(data_timeline)
 	var parseDate_Y = d3.timeParse("%Y");
 	var parseDate_MY = d3.timeParse("%b %Y");
 
 	//Data Parsing
-	data_education.forEach(function(d) {
-		d.start_date = parseDate_Y(d['Start Date']);
-		d.end_date = parseDate_Y(d['End Date']);
-		if (d.start_date.getTime() === d.end_date.getTime()) {
-			d.end_date = d.end_date + parseDate_Y("1");
-			console.log(d.end_date);
-		};
+	data_timeline.forEach(function(d) {
+		if (d.type==="education") {
+			d.start_date = parseDate_Y(d['Start Date']);
+			d.end_date = parseDate_Y(d['End Date']);
+			if (d.start_date.getTime() === d.end_date.getTime()) {
+				d.end_date = d.end_date + parseDate_Y("1");
+			//console.log(d.end_date);
+			};
+		}
+		if (d.type==="positions") {
+			d.start_date = parseDate_MY(d['Started On']);
+			d.end_date = parseDate_MY(d['Finished On']);
+		}
+		if (d.type==="projects") {
+			d.start_date = parseDate_MY(d['Start Date']);
+			d.end_date = parseDate_MY(d['End Date']);
+		}
+		
 	})
-	data_positions.forEach(function(d) {
-       d.start_date = parseDate_MY(d['Started On']);
-       d.end_date = parseDate_MY(d['Finished On']);
-	})
-	data_projects.forEach(function(d) {
-       d.start_date = parseDate_MY(d['Start Date']);
-       d.end_date = parseDate_MY(d['End Date']);
-	})
+
 	// Creation du svg graph
 	var margin = {
 	top: 5,
@@ -41,58 +60,55 @@ function plot_timeline(data_timeline) {
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	//Echelle de couleur
-	var color = d3.scaleOrdinal(d3.schemeCategory10)
+	var color = d3.scaleOrdinal(d3.schemePaired)
 
 	//echelle verticale y
-	var min_list = [];
-	var max_list = [];
-	min_list.push(d3.min(data_education, function(d) {
-	return d.start_date
-	}));
-	min_list.push(d3.min(data_positions, function(d) {
-	return d.start_date
-	}));
-	min_list.push(d3.min(data_projects, function(d) {
-	return d.start_date
-	}));
-
-	max_list.push(d3.max(data_education, function(d) {
-	return d.end_date
-	}));
-	max_list.push(d3.max(data_positions, function(d) {
-	return d.end_date
-	}));
-	max_list.push(d3.max(data_projects, function(d) {
-	return d.end_date
-	}));
-
 	var yScale = d3.scaleTime()
-	.domain([d3.min(min_list, function(d) {
-	  return d
-	}), d3.max(max_list, function(d) {
-	  return d
+	.domain([d3.min(data_timeline, function(d) {
+	  return d.start_date
+	}), d3.max(data_timeline, function(d) {
+	  return d.end_date
 	})])
 	.range([height, 0]);
 
 	//echelle x
 	var xScale = d3.scaleBand()
-	.domain([0, 1, 2])
+	.domain(["education", "positions", "projects"])
 	.rangeRound([0, width])
 	.paddingInner(0.5);
 	
+	//rect
 	svg.append('g').selectAll("rect")
-	.data(data_education)
+	.data(data_timeline)
 	.enter()
 		.append("rect")
-			.attr("x", xScale(0))
+			.attr("x", function(d) {return xScale(d.type)})
 			.attr("y",function(d){return yScale(d.end_date);})
 			.attr("height",function(d){
 				//console.log(yScale(d.end_date));
 				return yScale(d.start_date)-yScale(d.end_date);
 			})
 			.attr("width", function(d,i){return xScale.bandwidth();})
-			.attr("fill", "none")
-			.attr('stroke', function(d,i) {return color(i);})
-			.attr("transform", function(d,i) {return "translate(" + 10*i + ", 0 )"});
+			.attr("fill", function(d,i) {return color(i);})
+			.attr('stroke', "black")
+	//text
+	var text_box = d3.select("#timeline_text").selectAll("div")
+	.data(data_timeline)
+	.enter()
+		.append("div")
+		.attr("class", "text_box");
+	text_box.append("div")//title
+		.attr("class", "title")
+		.text(function(d) {
+			if (d.type==="education") {
+				return d['School Name'];
+			}
+			if (d.type==="positions") {
+				return d['Company Name'];
+			}
+		});
+	text_box.append("div")//subtitle
+		.attr("class", "subtitle")
+		.text(function(d) {return d['Notes']});
 	
 }
